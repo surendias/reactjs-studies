b1f7074775bb0ff59930b708b282befaa426b479
Suren Dias <suren.dias87@gmail.com>
2021-12-30 22:06:08 +0530

    Chapter 9: Controlled Component Inputs

--- dave-gray-tute/01tut/src/AddItem.js
+++ dave-gray-tute/01tut/src/AddItem.js
@@ -18,7 +18,11 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
