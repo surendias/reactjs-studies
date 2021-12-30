9f1db26cc1e820dfcdd214be55f1c4831f4a239c
Suren Dias <suren.dias87@gmail.com>
2021-12-30 22:02:41 +0530

    Chapter 9: Controlled Component Inputs

---
+++ dave-gray-tute/01tut/src/AddItem.js
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import { FaPlus } from "react-icons/fa";
+
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
+
+  return (
+    <form className="addForm" onSubmit={handleSubmit}>
+      <label htmlFor="addItem">Add Item</label>
+      <input
+        autoFocus
+        ref={inputRef}
+        type="text"
+        name="addItem"
+        id="addItem"
+        placeholder="Add Item"
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button type="submit" aria-label="Add Item">
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
+
+export default AddItem;
--- dave-gray-tute/01tut/src/App.js
+++ dave-gray-tute/01tut/src/App.js
@@ -1,48 +1,67 @@
 import React, { useState } from "react";
 import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "Milk",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Bacon",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Eggs",
-    },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
+
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  function setAndSaveItems(listItems) {
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     console.log(`key: ${id}`);
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     console.log(id);
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+
+    addItem(newItem);
+    setNewItem("");
+    console.log("submitted");
   };
 
   return (
     <div className="App">
       <Header title="Grocery List" />
+
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
